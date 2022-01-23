c2c91dbfd9846e15d5cc03dc82437f4f72f9a446
jaeyeon <jae518@naver.com>
2022-01-24 00:15:57 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // message 초기값을 ""으로 설정.
+  const [message, setMessage] = useState("");
+  // useEffect(함수,배열) : 컴포넌트가 화면에 나타났을(마운트)때 자동 실행.
+  useEffect(() => {
+    // fetch(url,options) : HTTP 요청 함수
+    fetch("/demo/hello")
+      .then((response) => response.text())
+      .then((message) => {
+        setMessage(message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +22,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <h1 className="App-title">{message}</h1>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
